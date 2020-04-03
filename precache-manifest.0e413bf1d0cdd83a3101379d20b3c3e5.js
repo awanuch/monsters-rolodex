9d78fe24f575a1f063078ca7815416c4156186bc
awanuch <62755082+awanuch@users.noreply.github.com>
2020-04-02 20:27:45 -0400

    Updates

---
+++ precache-manifest.0e413bf1d0cdd83a3101379d20b3c3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339fe97225d7f50e46ac2fd83d62530a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "20bb0a881f7fcba3f2ae",
+    "url": "/monsters-rolodex/static/css/main.ebd804db.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20bb0a881f7fcba3f2ae",
+    "url": "/monsters-rolodex/static/js/main.a9425f62.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
